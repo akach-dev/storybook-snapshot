38391c80d0e587c84b705096bc2aa7bfbaacac92
Alexander Kachan <akach.dev@mail.ru>
2023-10-10 18:38:46 +0300

    Task.stories done

---
+++ src/stories/Task.stories.tsx
@@ -0,0 +1,69 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {action} from '@storybook/addon-actions'
+
+import {Task} from "../Task";
+import {TaskType} from "../Todolist";
+import {useState} from 'react';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof Task> = {
+  title: 'TODOLISTS/Task',
+  component: Task,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: '',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    changeTaskStatus: {
+      description: 'changeTaskStatus description',
+      action: 'changeTaskStatus action'
+    },
+    removeTask: {
+      description: 'removeTask description',
+      action: 'removeTask action'
+    },
+    changeTaskTitle: {
+      description: 'changeTaskTitle description',
+      action: 'changeTaskTitle action'
+    },
+  },
+  args: {
+    task: {id: 'sdasdasd', title: 'JS', isDone: true},
+    todolistId: 'werwer',
+  }
+};
+
+export default meta;
+type Story = StoryObj<typeof Task>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+
+
+export const TaskIsDoneStory: Story = {};
+
+export const TaskIsNotDoneStory: Story = {
+  args: {
+    task: {id: 'sdasdasd', title: 'JS', isDone: false},
+  }
+};
+const TaskExample = () => {
+
+  const [task, setTask] = useState<TaskType>({id: 'sdasdasd', title: 'JS', isDone: true});
+
+  return <Task changeTaskStatus={() => setTask({...task, isDone: !task.isDone})}
+               changeTaskTitle={(title) => setTask({...task, title})}
+               removeTask={action('removeTask action')}
+               task={task}
+               todolistId={'qwewqrwetw'}
+  />
+
+};
+export const TaskStory: Story = {
+  render: () => <TaskExample/>
+};
+
+
+
