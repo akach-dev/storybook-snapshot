10b926b435f9fc48945a6606e04ec257963ee4c7
Alexander Kachan <akach.dev@mail.ru>
2023-10-10 19:08:35 +0300

    add AppWithRedux.stories

---
+++ src/stories/AppWithRedux.stories.tsx
@@ -0,0 +1,38 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {action} from '@storybook/addon-actions'
+
+import {Task} from "../Task";
+import {TaskType} from "../Todolist";
+import {useState} from 'react';
+import {EditableSpan} from "../EditableSpan";
+import AppWithRedux from "../AppWithRedux";
+import {Provider} from "react-redux";
+import {store} from "../state/store";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof AppWithRedux> = {
+  title: 'TODOLISTS/AppWithRedux',
+  component: AppWithRedux,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: '',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+};
+
+export default meta;
+type Story = StoryObj<typeof AppWithRedux>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+
+
+export const AppWithReduxStory: Story = {
+  render: () => <Provider store={store}><AppWithRedux/></Provider>
+};
+
+
+
+
+
